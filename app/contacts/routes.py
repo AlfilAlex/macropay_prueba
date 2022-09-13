f0f2b90516fcce33515d431d463a6e1b98d2013a
AlfilAlex <cabimax@outlook.com>
2022-09-13 15:49:28 -0500

    NF: Se agregó la vista de contacts con filtrado por argumento phrase

--- app/contacts/routes.py
+++ app/contacts/routes.py
@@ -1,9 +1,26 @@
 
-from flask import Blueprint
+from flask import Blueprint, make_response, request
 from flask import current_app as app
-
+from .. import contact_directory
 # from flask_cors import cross_origin
 
 api_prefix = app.config['PREFIX']
-user_profile = Blueprint('user_profile', __name__,
-                         url_prefix=api_prefix)
+contacts = Blueprint('contacts', __name__,
+                     url_prefix=api_prefix)
+
+
+@contacts.route('/contacts', methods=['GET'])
+def contacs():
+    if "phrase" in request.args:
+        if not request.args["phrase"]:
+            return make_response({"status": "fail", "data": {"phrase": "Empty phrases are not allowed"}}, 400)
+
+        dir_contacts = contact_directory.contacts_by_phrase(
+            request.args["phrase"])
+    else:
+        dir_contacts = contact_directory.all_contacts
+
+    dir_contacts.sort(key=lambda contact: contact["name"])
+    response = make_response({"status": "success",
+                              "data": {"contacts": dir_contacts}}, 200)
+    return response
